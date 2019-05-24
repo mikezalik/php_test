405220b7ca1a49eb3063b43a6d284cfc72f4655d
mikezalik <mike.zalik@gmail.com>
2019-05-23 22:54:47 -0700

    add, delete

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'vendor/autoload.php';
+# This logic handles connecting to the database, where we store our todo status
+$pdo = new \PDO("sqlite:" . "db/sqlite.db");
+
+# This PHP logic handles user actions
+# New TODO
+if (isset($_POST['submit'])) 
+{
+  $description = $_POST['description'];
+  $sth = $pdo->prepare("INSERT INTO todos (description) VALUES (:description)");
+  $sth->bindValue(':description', $description, PDO::PARAM_STR);
+  $sth->execute();
+}
+# Delete TODO
+elseif (isset($_POST['delete']))
+{ 
+  $id = $_POST['id'];
+  $sth = $pdo->prepare("delete from todos where id = :id");
+  $sth->bindValue(':id', $id, PDO::PARAM_INT);
+  $sth->execute();
+}
