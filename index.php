ffc60885b0defbaceff243ec870c2c9fbb5bc65d
mikezalik <mike.zalik@gmail.com>
2019-05-24 21:21:41 -0700

    HTML start

--- index.php
+++ index.php
@@ -28,3 +28,21 @@ elseif (isset($_POST['complete']))
     $sth->bindValue(':id', $id, PDO::PARAM_INT);
     $sth->execute();
 }
+?>
+<!--HTML-->
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <title>Todo List</title>
+</head>
+
+<body class="container">
+  <h1>Todo List</h1>
+  <form method="post" action="">
+    <input type="text" name="description" value="">
+    <input type="submit" name="submit" value="Add">
+  </form>
+  <h2>Current Todos</h2>
+  <table class="table table-striped">
+    <thead><th>Task</th><th></th><th></th></thead>
+    <tbody>
